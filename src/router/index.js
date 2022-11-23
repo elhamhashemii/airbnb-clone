0c3424350e76cee16ebf6f3ed569bbdfc39ab6d4
e.hashemi <e.hashemi@elanus.ir>
2022-11-23 13:41:40 +0330

    routes file added

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/Index.vue'
+import routes from "./routes";
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Index
-  },
-]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
