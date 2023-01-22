494631eb3f1c1e8be643767093d312db1cddca36
e.hashemi <e.hashemi@elanus.ir>
2023-01-22 17:30:53 +0330

    wip: adding images

---
+++ src/data/images.js
@@ -0,0 +1,23 @@
+export const imagesData = [
+    {id: 1, src: "https://a0.muscache.com/im/pictures/d8f415fe-dc35-4086-a129-c421575aa868.jpg?im_w=720"},
+    {id: 2, src: "https://a0.muscache.com/im/pictures/41d0f06f-0fab-474f-b920-9cf9163303a1.jpg?im_w=720"},
+    {id: 3, src: "https://a0.muscache.com/im/pictures/a8a5c738-eba2-418f-b7bf-55bfcbf79b55.jpg?im_w=720"},
+    {id: 4, src: "https://a0.muscache.com/im/pictures/99231d05-c80f-4f34-8d86-c263f081d44d.jpg?im_w=720"},
+    {id: 5, src: "https://a0.muscache.com/im/pictures/233cad88-0249-4a80-9af9-48ee571a5716.jpg?im_w=720"},
+    {id: 6, src: "https://a0.muscache.com/im/pictures/5785c25c-228e-41bd-9e06-a5cadebceba0.jpg?im_w=720"},
+    {id: 7, src: "https://a0.muscache.com/im/pictures/305389a0-7893-4db5-b0b4-a7ee72bc8817.jpg?im_w=720"},
+    {id: 8, src: "https://a0.muscache.com/im/pictures/c3f1d267-89db-40ac-92a9-8cea414f9ffb.jpg?im_w=720"},
+    {id: 9, src: "https://a0.muscache.com/im/pictures/56dd4335-57f0-4e32-a823-f00cc2a73589.jpg?im_w=720"},
+    {id: 10, src: "https://a0.muscache.com/im/pictures/444a8225-e657-4d62-97db-42f7423ae890.jpg?im_w=720"},
+    {id: 11, src: "https://a0.muscache.com/im/pictures/d09b3987-7da3-4ddc-a0e8-32f0ff37c2d5.jpg?im_w=720"},
+    {id: 12, src: "https://a0.muscache.com/im/pictures/e8a898be-63e0-41d4-947a-4a925ec78c09.jpg?im_w=720"},
+    {id: 13, src: "https://a0.muscache.com/im/pictures/56dd4335-57f0-4e32-a823-f00cc2a73589.jpg?im_w=720"},
+    {id: 14, src: "https://a0.muscache.com/im/pictures/90de5b70-278e-44b8-9452-3871be0555a9.jpg?im_w=720"},
+    {id: 15, src: "https://a0.muscache.com/im/pictures/7a537b27-1f79-40f7-85a2-58e834c067d4.jpg?im_w=720"},
+    {id: 16, src: "https://a0.muscache.com/im/pictures/186c0318-0a61-4707-b1e2-6eec6a4c9edf.jpg?im_w=720"},
+    {id: 17, src: "https://a0.muscache.com/im/pictures/0c3f751e-6823-4b0d-a324-554f217f15a9.jpg?im_w=720"},
+    {id: 18, src: "https://a0.muscache.com/im/pictures/3e3de927-eb4c-476b-9315-c7f366497909.jpg?im_w=720"},
+    {id: 19, src: "https://a0.muscache.com/im/pictures/4a5c629b-9c92-450e-8d8f-995875798838.jpg?im_w=720"},
+    {id: 20, src: "https://a0.muscache.com/im/pictures/f8a1418d-e234-48f4-b856-fcecd1e5b41b.jpg?im_w=720"}
+
+]
