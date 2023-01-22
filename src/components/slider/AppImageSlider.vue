8664d5f66fc6546cf5e7098458f5b668092f6a8f
e.hashemi <e.hashemi@elanus.ir>
2023-01-22 17:31:34 +0330

    wip: images improved

--- src/components/slider/AppImageSlider.vue
+++ src/components/slider/AppImageSlider.vue
@@ -1,15 +1,28 @@
 <template>
-    <!-- Image Slider -->
-    <Swiper class="image-container rounded-xl" :pagination="true" :modules="modules" >
-        <SwiperSlide v-for="img in images" :key="img.id">
+    <div>
+        <div>
+            <Swiper class="image-container rounded-xl" :pagination="true" :modules="modules" >
+        <SwiperSlide v-for="img in props.images" :key="img.id">
             <div v-if="loading" class="w-full h-full bg-gray-200"></div>
-            <img v-else :src="img.src" class="w-full h-full" alt="">
+            <img v-else :src="img.src" class="w-full h-full" :alt="img.id">
         </SwiperSlide>
     </Swiper>
+        </div>
+        <div class="text-start my-5 w-full content">
+            <div class="flex flex-row justify-between items-center">
+                <div class="text-black text-sm font-bold">Defiance, Missouri</div>
+                <div class="text-black text-sm font-bold">
+                    <font-awesome-icon icon="star"></font-awesome-icon>
+                    <span class="mx-1">4.6</span>
+                </div>
+            </div>
+            <div class="text-gray-400 my-2 text-xs">41 miles to Gateway Arch National Park Jan 8 – 13</div>
+            <div class="text-sm"><span class="font-bold text-black">€</span> 171 night</div>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Pagination } from "swiper";import 'swiper/css';
 import "swiper/css/pagination";
@@ -21,18 +34,15 @@ const modules = [Pagination];
 //     loading.value = false
 // }, 2500);
 
-const images = [
-    {id: 0, src: require('@/assets/images/1.webp')},
-    {id: 1, src: require('@/assets/images/2.webp')},
-    {id: 2, src: require('@/assets/images/3.webp')},
-    {id: 3, src: require('@/assets/images/4.webp')},
-]
+const props = defineProps(['images'])
+
+
 </script>
 
 
 <style lang="scss" scoped>
 .image-container {
-    width: 20rem;
-    height: 20rem;
+    width: 16rem;
+    height: 16rem;
 }
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,7 +8,7 @@
         </div>
     </div>
     <div class="my-5 flex flex-wrap justify-between items-center">
-        <AppHomeCard v-for="item in 21" class="w-2/10"></AppHomeCard>
+        <AppImageSlider v-for="item in data" class="w-2/12" :images="item"></AppImageSlider>
     </div>
 </template>
 
@@ -16,12 +16,40 @@
 import { ref } from "vue"
 import AppIconsSlider from '@/components/slider/AppIconsSlider.vue';
 import AppFilterBtn from '@/components/btn/AppFilterBtn.vue'
-import AppHomeCard from '@/components/cards/AppHomeCard.vue';
-
+import AppImageSlider from '@/components/slider/AppImageSlider.vue';
+import { imagesData } from "@/data/images"
 const screen = ref(window.screen.width)
 
 window.addEventListener('resize', (e) => {
     screen.value = e.target.innerWidth
 })
+
+const sliderImages = imagesData;
+
+const images = ref([])
+
+// Data is an array of arrays
+const data = ref([])
+
+function generateNewInstances () {
+    for (let i = 0; i < 21; i++) {
+    images.value = sliderImages.sort((a,b) => 0.5 - Math.random()).slice(0,4)
+    data.value.push(images.value)
+    }
+}
+
+generateNewInstances()
+
+const api = require('axios');
+
+const apiData = ref()
+
+
+api
+    .get("https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&facet=host_response_time&facet=host_response_rate&facet=host_verifications&facet=city&facet=country&facet=property_type&facet=room_type&facet=bed_type&facet=amenities&facet=availability_365&facet=cancellation_policy&facet=features")
+    .then((res) => {
+        apiData.value = res.data.records
+        console.log(res.data.records);
+    });
 
 </script>
